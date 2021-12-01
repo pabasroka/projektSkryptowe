3037b5d074191780baf650bcce1fe9d0af95a891
Patryk <patrykyrtap@o2.pl>
2021-12-01 12:51:36 +0100

    init repo

---
+++ app.bat
@@ -0,0 +1,41 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:menu
+echo =============
+echo 1. Start
+echo 2. Informacje
+echo 3. Backup
+echo 4. Zakoncz
+echo =============
+
+set /p input=Wybierz opcje: 
+if %input% EQU 1 goto start
+if %input% EQU 2 goto info
+if %input% EQU 3 goto backup
+if %input% EQU 4 (
+    exit
+) else (
+    cls
+    goto menu
+) 
+
+:start
+call app.py
+call web.py
+
+:info 
+echo Program wykonuje podstawowe dzialania arytmetyczne w dziedzinie liczb zespolonych
+echo Program pobiera dane wejsciowe z pliku
+echo a nastepnie zwraca wynik w postaci strony
+echo Program jest zadaniem konkursowym z konkursu Algorytmion 2017 
+echo Autor: Patryk Sroczynski
+set /p c=Nacisnij enter
+cls 
+goto menu
+
+:backup
+mkdir backup%date%
+xcopy %cd% \backup /S
+
+pause
---
+++ app.py
@@ -0,0 +1,90 @@
+import math
+
+class Complex(object):
+
+    def __init__(self, real, imag = 0.0):
+        self.real = real
+        self.imag = imag
+
+    def __add__(self, other):
+        return Complex(self.real + other.real, self.imag + other.imag)
+
+    def __sub__(self, other):
+        return Complex(self.real - other.real, self.imag - other.imag)
+    
+    def __mul__(self, other):
+        return Complex((self.real * other.real) - (self.imag * other.imag),
+            (self.imag * other.real) + (self.real * other.imag))
+
+    def __truediv__(self, other):
+        m = other.real ** 2 + other.imag ** 2
+        return Complex((self.real * other.real + self.imag * other.imag) / m, 
+            (self.imag * other.real - self.real * other.imag) / m)
+
+    def __pow__(self, n):
+        return Complex((self.real + self.imag)**n)
+        # r = math.sqrt(self.real**2 + self.imag**2)
+        # cosf = self.real / r
+        # sinf = self.imag / r
+        # if sinf > 0 cosf > 0:
+        #     f = 
+        # return Complex(self.real**n, self.imag**n)
+
+
+numbers = []
+try:
+    file = open("in.txt", "r")
+    for line in file.readlines():
+        try:
+            numbers.append(float(line))
+        except ValueError:
+            numbers.append(1)
+    file.close()
+    # x1 = numbers[0]
+    # x2 = numbers[1]
+    # i1 = numbers[2]
+    # i2 = numbers[3]
+    # n = numbers[4]
+    try:
+        x1 = numbers[0]
+    except IndexError:
+        x1 = 1
+    try:
+        i1 = numbers[1]
+    except IndexError:
+        i1 = 1
+    try:
+        x2 = numbers[2]
+    except IndexError:
+        x2 = 1
+    try:
+        i2 = numbers[3]
+    except IndexError:
+        i2 = 1
+    try:
+        n = numbers[4]
+    except IndexError:
+        n = 1
+
+    a = Complex(x1, i1)
+    b = Complex(x2, i2)
+    sum = a + b
+    dif = a - b
+    prod = a * b
+    quo = a / b
+    pow = Complex(3, 2)**3
+
+except IOError:
+    print("Blad otwierania pliku")
+
+file = open("out.txt", "w")
+data = str(sum.real) + "\n" + str(sum.imag) + "\n" + str(dif.real) + "\n" + str(dif.imag) + "\n" + str(prod.real) + "\n" + str(prod.imag) + "\n" + str(quo.real) + "\n" + str(quo.imag) + "\n" + str(pow.real) + "\n" + str(pow.imag)
+file.write(data)
+file.close()
+
+
+print(sum.real, sum.imag)
+print(dif.real, dif.imag)
+print(prod.real, prod.imag)
+print(quo.real, quo.imag)
+print(pow.real, pow.imag)
